74eb529c8e5a50f8c40ebf49ce9075504de91cdf
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-24 17:45:36 +0200

    setup tailwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,9 @@
   <title>Verite's Site</title>
 </svelte:head>
 
-<h1>Hi I'm Makuza Mugabo Verite</h1>
+<section class="bg-red-500 h-screen w-screen">
+  <h1>Hi I'm Makuza Mugabo Verite</h1>
+</section>
 
 <style>
 </style>
