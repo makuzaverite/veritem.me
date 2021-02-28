75864d8b3090614c6a29052ebf923082aeb7315f
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-28 10:24:02 +0200

    updateed moile responsiveness

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,8 +33,10 @@
 <section
   class="bg-primary h-screen w-screen flex justify-center flex-col items-center font-sans"
 >
-  <div class="w-1/2">
-    <h2 class="text-gray-100  text-5xl mb-8 font-bold capitalize">
+  <div class="w-4/5 md:w-1/2 sm:w-4/5">
+    <h2
+      class="text-gray-100  text-3xl md:text-5xl sm:text-4xl mb-8 font-bold capitalize"
+    >
       Hi! I'm Makuza Mugabo verite
     </h2>
     <p class="text-base sm:text-base md:text-lg text-gray-400 mb-4">
