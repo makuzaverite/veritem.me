d9fc765e4f23125d997d3617931ca921e84a9c05
veritem <mugaboverite@gmail.com>
2021-04-23 19:14:45 +0200

    feat: setup

---
+++ src/routes/gh.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { onMount } from 'svelte';
+
+	// async function preload(page, session) {
+	// 		return this.redirect(301, 'https://github.com/makuzaverite');
+	// 	}
+
+	onMount(() => {
+		window.location.replace('https://github.com/makuzaverite');
+	});
+</script>
